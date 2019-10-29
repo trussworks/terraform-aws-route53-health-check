75d1215c65cbdf35b323ffd35db2ba48a012ab13
Rupinder Dhariwal <rd@truss.works>
2019-10-29 16:04:49 -0400

    Upgrade to TF 12

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
  *
  * ```hcl
  * module "route53_health_check" {
- *   source = "./route53-health-check"
+ *   source = "trussworks/route53-health-check/aws"
  *
  *   environment       = "${var.environment}"
  *   dns_name          = "${local.my_move_dns_name}"
@@ -16,41 +16,41 @@
  */
 
 resource "aws_route53_health_check" "http" {
-  fqdn              = "${var.dns_name}"
+  fqdn              = var.dns_name
   port              = 80
   type              = "HTTP"
-  resource_path     = "${var.health_check_path}"
-  failure_threshold = "${var.failure_threshold}"
-  request_interval  = "${var.request_interval}"
-  regions           = "${var.health_check_regions}"
+  resource_path     = var.health_check_path
+  failure_threshold = var.failure_threshold
+  request_interval  = var.request_interval
+  regions           = var.health_check_regions
   measure_latency   = true
 
   tags = {
     Name        = "${var.dns_name}-http"
-    Environment = "${var.environment}"
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
 
 resource "aws_route53_health_check" "https" {
-  fqdn              = "${var.dns_name}"
+  fqdn              = var.dns_name
   port              = 443
   type              = "HTTPS"
-  resource_path     = "${var.health_check_path}"
-  failure_threshold = "${var.failure_threshold}"
-  request_interval  = "${var.request_interval}"
-  regions           = "${var.health_check_regions}"
+  resource_path     = var.health_check_path
+  failure_threshold = var.failure_threshold
+  request_interval  = var.request_interval
+  regions           = var.health_check_regions
   measure_latency   = true
 
   tags = {
     Name        = "${var.dns_name}-https"
-    Environment = "${var.environment}"
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "http" {
-  provider = "aws.us-east-1"
+  provider = aws.us-east-1
 
   alarm_name        = "${var.dns_name}-status-http"
   alarm_description = "Route53 health check status for ${var.dns_name}"
@@ -58,8 +58,8 @@ resource "aws_cloudwatch_metric_alarm" "http" {
   namespace   = "AWS/Route53"
   metric_name = "HealthCheckStatus"
 
-  dimensions {
-    HealthCheckId = "${aws_route53_health_check.http.id}"
+  dimensions = {
+    HealthCheckId = aws_route53_health_check.http.id
   }
 
   comparison_operator = "LessThanThreshold"
@@ -69,13 +69,13 @@ resource "aws_cloudwatch_metric_alarm" "http" {
   evaluation_periods = "1"
   period             = "60"
 
-  alarm_actions             = ["${var.alarm_actions}"]
-  ok_actions                = ["${var.alarm_actions}"]
-  insufficient_data_actions = ["${var.alarm_actions}"]
+  alarm_actions             = var.alarm_actions
+  ok_actions                = var.alarm_actions
+  insufficient_data_actions = var.alarm_actions
 }
 
 resource "aws_cloudwatch_metric_alarm" "https" {
-  provider = "aws.us-east-1"
+  provider = aws.us-east-1
 
   alarm_name        = "${var.dns_name}-status-https"
   alarm_description = "Route53 health check status for ${var.dns_name}"
@@ -83,8 +83,8 @@ resource "aws_cloudwatch_metric_alarm" "https" {
   namespace   = "AWS/Route53"
   metric_name = "HealthCheckStatus"
 
-  dimensions {
-    HealthCheckId = "${aws_route53_health_check.https.id}"
+  dimensions = {
+    HealthCheckId = aws_route53_health_check.https.id
   }
 
   comparison_operator = "LessThanThreshold"
@@ -94,7 +94,8 @@ resource "aws_cloudwatch_metric_alarm" "https" {
   evaluation_periods = "1"
   period             = "60"
 
-  alarm_actions             = ["${var.alarm_actions}"]
-  ok_actions                = ["${var.alarm_actions}"]
-  insufficient_data_actions = ["${var.alarm_actions}"]
+  alarm_actions             = var.alarm_actions
+  ok_actions                = var.alarm_actions
+  insufficient_data_actions = var.alarm_actions
 }
+
--- variables.tf
+++ variables.tf
@@ -1,38 +1,39 @@
 variable "environment" {
   description = "Environment tag (e.g. prod)."
-  type        = "string"
+  type        = string
 }
 
 variable "dns_name" {
   description = "Fully-qualified domain name (FQDN) to create."
-  type        = "string"
+  type        = string
 }
 
 variable "alarm_actions" {
   description = "Actions to execute when this alarm transitions."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "health_check_regions" {
   description = "AWS Regions for health check"
-  type        = "list"
+  type        = list(string)
   default     = ["us-east-1", "us-west-1", "us-west-2"]
 }
 
 variable "health_check_path" {
   description = "Resource Path to check"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "failure_threshold" {
   description = "Failure Threshold (must be less than or equal to 10)"
-  type        = "string"
+  type        = string
   default     = "3"
 }
 
 variable "request_interval" {
   description = "Request Interval (must be 10 or 30)"
-  type        = "string"
+  type        = string
   default     = "30"
 }
+

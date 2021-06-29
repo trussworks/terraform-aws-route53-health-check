f4aae1f183ff665a44fed67039b28bbcec610e97
Joshua Sarnovsky <jsarnovsky@truss.works>
2021-06-29 17:20:01 -0400

    fix

--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 3.0"
-      configuration_aliases = [aws.us-east-1]
-    }
+    aws = ">= 3.0"
   }
+}
+provider "aws" {
+  alias = "us-east-1"
 }
